e9c7560dfa54eb256d809aa38ad702987bee6df6
Bernard <bsuwirjo@gmail.com>
2020-01-22 11:06:05 -0800

    Split into  modular program

--- learning/reversePy/new/createData.py
+++ learning/reversePy/new/createData.py
@@ -1,16 +1,17 @@
 import sqlite3
+import sys
 from pyteomics import mass as massC
 import struct
 import numpy as np
 import pandas as pd
 
-import keras.backend as K
-from keras.layers.convolutional import Conv1D
-from keras.layers.core import Dense, Dropout, Masking
-from keras.layers.recurrent import LSTM
-from keras.layers.wrappers import Bidirectional, TimeDistributed
-from keras.models import load_model as keras_load_model
-from keras.models import Sequential
+
+if len(sys.argv) != 4:
+    print("Error with command line inputs")
+else:
+    DATABASE_PATH = sys.argv[1]
+    CHARGE2_PATH = sys.argv[2]
+    CHARGE3_PATH = sys.argv[3]
 
 #Binary Search for elements in mass Spectra within Certain Tolerance
 def binarySearch (arr, l, r, x, tol):
@@ -68,12 +69,6 @@ def getIonMasses(peptide, types=('b', 'y'), maxcharge=2):
                         ions[key].append( massC.fast_mass(peptide[i:], ion_type=ion_type, charge=charge))
     return ions
 
-if len(sys.argv) != 4:
-    print("Error with command line inputs")
-else:
-    DATABASE_PATH = sys.argv[1]
-    CHARGE2_PATH = sys.argv[2]
-    CHARGE3_PATH = sys.argv[3]
 
 
 #Connect to database
@@ -199,169 +194,3 @@ for i in df["ions"]:
 
 c2DF.to_pickle(CHARGE2_PATH)
 c3DF.to_pickle(CHARGE3_PATH)
-# In[7]:
-
-
-df = c2DF
-
-
-#Encode Sequence
-
-#read the matrix a csv file on github
-nlf = pd.read_csv('https://raw.githubusercontent.com/dmnfarrell/epitopepredict/master/epitopepredict/mhcdata/NLF.csv',index_col=0)
-
-def show_matrix(m):
-    #display a matrix
-    cm = sns.light_palette("seagreen", as_cmap=True)
-    display(m.style.background_gradient(cmap=cm))
-
-def nlf_encode(seq):
-    x = pd.DataFrame([nlf[i] for i in seq]).reset_index(drop=True)
-    #show_matrix(x)
-    e = x.values.flatten()
-    return e
-
-modPeptides = c2DF['peptide'].copy()
-for i, pep in enumerate(modPeptides):
-    modPeptides.iloc[i] = nlf_encode(pep)
-
-"""
-for i in modPeptides:
-    print(i.shape)
-"""
-
-#m odPeptides.head()
-#print(modPeptides.iloc[0])
-
-
-# import seaborn as sns
-# codes = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
-#          'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
-# #Encode Sequence
-# def show_matrix(m):
-#     #display a matrix
-#     cm = sns.light_palette("seagreen", as_cmap=True)
-#     display(m.style.background_gradient(cmap=cm))
-#
-# def one_hot_encode(seq):
-#     o = list(set(codes) - set(seq))
-#     s = pd.DataFrame(list(seq))
-#     x = pd.DataFrame(np.zeros((len(seq),len(o)),dtype=int),columns=o)
-#     a = s[0].str.get_dummies(sep=',')
-#     a = a.join(x)
-#     a = a.sort_index(axis=1)
-#     #show_matrix(a)
-#     e = a.values.flatten()
-#     return e
-#
-# modPeptides = df['peptide'].copy()
-# for i, pep in enumerate(modPeptides):
-#     modPeptides.iloc[i] = one_hot_encode(pep)
-#
-# """
-# for i in modPeptides:
-#     print(i.shape)
-# """
-#
-# modPeptides.head()
-# print(modPeptides.iloc[0])
-
-# In[8]:
-
-
-df = pd.concat([df, modPeptides], axis = 1)
-df.columns = ['sequence', 'ions', 'encodedSequence']
-df = df[['sequence', 'encodedSequence', 'ions']]
-df.head()
-print(np.array(df['encodedSequence'].iloc[0]).shape)
-
-
-# In[9]:
-
-
-ionsProcessed = df['ions'].copy()
-for i, ionDict in enumerate(ionsProcessed):
-    container  = []
-    for key,value in ionDict.items():
-        ionList = []
-        for j in value:
-            ionList.append(j)
-        container.append(np.array(ionList))
-    ionsProcessed.iloc[i] = np.array(container)
-    #print(np.array(container).shape)
-#ionsProcessed.head()
-
-df = pd.concat([df, ionsProcessed], axis = 1)
-df.columns = ['sequence', 'encodedSequence', 'ions', 'ionsProcessed']
-print(df.head())
-
-
-# In[10]:
-
-
-
-inputArr = np.array(df['encodedSequence'])
-inputArr = np.array([np.array(i) for i in inputArr])
-outputArr = np.array([np.array(i) for i in df['ionsProcessed']])
-fragmentShape =  outputArr.shape[1]*outputArr.shape[2]
-
-print(outputArr.shape)
-inputArr = np.reshape(inputArr, (inputArr.shape[0], 1, inputArr.shape[1]))
-outputArr = np.reshape(outputArr, (outputArr.shape[0],fragmentShape))
-
-splitIdx = int(len(inputArr) * .1)
-
-xTrain = inputArr[:splitIdx]
-xTest = inputArr[splitIdx:]
-
-yTrain = outputArr[:splitIdx]
-yTest = outputArr[splitIdx:]
-print(inputArr.shape)
-print(outputArr.shape)
-
-
-# In[11]:
-
-
-model = Sequential()
-
-model.add(Bidirectional(LSTM(3, input_shape = xTrain.shape)))
-model.add(Dense(100))
-model.add(Dropout(0.5))
-#model.add(TimeDistributed(Dense(fragmentShape, activation='relu')))
-model.add(Dense(fragmentShape, activation='relu'))
-
-model.compile(
-    loss="mean_squared_error",
-    optimizer="adam")
-model.fit(xTrain, yTrain, epochs = 5000)
-
-
-# In[12]:
-
-
-predictions = model.predict(xTest)
-"""
-for i in predictions:
-    ionContainer
-    while """
-
-
-# In[13]:
-
-
-def printResults(dictionary, array):
-    print("--------------------")
-    j = 0
-    for key, value in dictionary.items():
-        for i in value:
-            print(i, " | ", array[j])
-            j += 1
-
-for i, val in enumerate(predictions):
-    dictionary = df.iloc[i]["ions"]
-    printResults(dictionary, val)
-
-
-
-# In[ ]:
